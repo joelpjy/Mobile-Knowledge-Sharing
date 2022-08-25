3009c31eb729f301b32319a75dcf7bfe593c9de1
Joel <joelp@netvirta.com>
2022-08-25 17:34:16 +0800

    feat: integrate crashlytics for both ios and android

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'dart:async';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mobile_knowledge_sharing_app/app/app.dart';
@@ -10,6 +9,10 @@ const bool isProduction = bool.fromEnvironment('dart.vm.product');
 void main() async {
   // execute blocking functions only after binding completes
   WidgetsFlutterBinding.ensureInitialized();
+
+  print("123123");
+  await Firebase.initializeApp();
+  print("456456456");
 
   // pre-launch setup and config
   setupLocator();
--- lib/ui/views/home/home_view_model.dart
+++ lib/ui/views/home/home_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:mobile_knowledge_sharing_app/app/config.locator.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -6,6 +7,7 @@ class HomeViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
 
   Future initialise() async {
-
+    //throw ("HELP MOBILE PLS");
+    FirebaseCrashlytics.instance.crash();
   }
 }
